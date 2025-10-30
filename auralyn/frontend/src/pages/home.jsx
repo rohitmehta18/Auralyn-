d22a39ef8f177bf4b1e7e3f9059d0e01de09e9b8
rohitmehta18 <rmehta1836@gmail.com>
2025-10-30 14:00:49 +0530

    basic nav bar and home page

---
+++ auralyn/frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1 className="title">Welcome to Auralyn </h1>
+      <p className="subtitle">Feel the rhythm. Feel the soul.</p>
+      <button className="explore-btn">Explore Music</button>
+    </div>
+  );
+}
